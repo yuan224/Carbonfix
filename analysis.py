622dd6c31c2bd5e8770d5bcf6b09f239798aaf75
yuan224 <ryan91.kuo@gmail.com>
2025-12-08 00:08:18 +0800

    first_upload

---
+++ analysis.py
@@ -0,0 +1,442 @@
+import aspen_tools
+import pandas as pd
+import numpy as np
+import time
+import pythoncom
+from openpyxl import load_workbook
+from openpyxl.styles import Font, Border, Side
+
+def calc_Tb(Aspen,Asp, T_list,P_list, NCOMP):
+    Tl = T_list[0]
+    Tu = T_list[-1]
+    Tb_list = []
+    Asp.set_focus()
+    aspen_tools.cd_properties(Asp)
+    Asp.child_window(title="PURE-G", control_type="TreeItem").click_input()
+    for Pi, P in enumerate(P_list):
+        n = 200
+        Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-G\Input\PLIST\#0").Value = P
+        Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-G\Input\TLOWER").Value = Tl
+        Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-G\Input\TUPPER").Value = Tu
+        Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-G\Input\TNPOINT").Value = n
+        aspen_tools.run(Asp)
+        time.sleep(2)  
+        result = [0] * NCOMP  # 對每個成分記錄沸點或狀態
+        for k in range(NCOMP):
+            for i in range(n + 1):
+                lG = Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-G\Output\Prop Data\PROPTAB").Elements.Item(f"LIQUID G COMP{k+1}", str(i+1)).Value
+                vG = Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-G\Output\Prop Data\PROPTAB").Elements.Item(f"VAPOR G COMP{k+1}", str(i+1)).Value
+                if lG > vG and i == 0:
+                    result[k] = "TB is under Tl"
+                    break
+                elif lG >= vG:
+                    result[k] = round(Tl + i * (Tu - Tl) / n, 2)
+                    break
+
+            if i == n and result[k] == 0:
+                result[k] = "TB is above Tu"
+        Tb_list.append(result)
+    format_Tb_table(Tb_list, P_list, Tl, Tu)
+    return Tb_list
+
+def format_Tb_table(Tb_list, P_list, Tl, Tu):
+    comp_aliases = [f"COMP{i+1}" for i in range(len(Tb_list[0]))]
+
+    col_w = 18
+    left_w = 14
+    total_w = left_w + (len(comp_aliases) * (col_w + 3))
+
+    print("=" * total_w)
+    header = f"{'Pressure':<{left_w}}" + "".join([f"| {alias:<{col_w}}" for alias in comp_aliases])
+    print(header)
+    print("=" * total_w)
+
+    for p, row in zip(P_list, Tb_list):
+        line = f"{f'{p} atm':<{left_w}}"
+        for v in row:
+            if isinstance(v, str):
+                if "under Tl" in v:
+                    v = f"< {Tl} K"
+                elif "above Tu" in v:
+                    v = f"> {Tu} K"
+            line += f"| {str(v) :<{col_w}} K"
+        print(line)
+
+    print("=" * total_w)
+
+
+
+def Bpure(Aspen, Asp, T_list, P_list, Tb_list, n_list,NCOMP,O = False):
+    Tl = T_list[0]
+    Tu = T_list[-1]
+    rxnB_list = []
+    aspen_tools.cd_properties(Asp)
+    # Asp.set_focus()
+    # Asp.child_window(title="PURE-B", control_type="TreeItem").click_input()
+    index = 0
+    Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-B\Input\TLOWER").Value = Tl
+    Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-B\Input\TUPPER").Value = Tu
+    Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-B\Input\TNPOINT").Value = len(T_list) - 1
+    for Pi, P in enumerate(P_list):
+        rxnB = []
+        Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\PURE-B\Input\PLIST\#0").Value = P
+        aspen_tools.run(Asp)
+
+        for n, T in enumerate(T_list):
+            B = []
+            rB = 0
+
+            for i in range(NCOMP):
+                vB = Aspen.Application.Tree.FindNode(
+                    r"\Data\Properties\Analysis\PURE-B\Output\Prop Data\PROPTAB"
+                ).Elements.Item(f"VAPOR AVAIL COMP{i+1}", str(n + 1)).Value
+
+                lB = Aspen.Application.Tree.FindNode(
+                    r"\Data\Properties\Analysis\PURE-B\Output\Prop Data\PROPTAB"
+                ).Elements.Item(f"LIQUID AVAIL COMP{i+1}", str(n + 1)).Value
+
+                Tb = Tb_list[Pi][i]
+                if Tb == "TB is under Tl":
+                    B.append(vB)
+                elif Tb == "TB is above Tu":
+                    B.append(lB)
+                elif T >= Tb:
+                    B.append(vB)
+                else:
+                    B.append(lB)
+            if O and n==0 and Pi ==0:#算n_out_Bp0
+                B0 = B
+            for i in range(NCOMP):
+                if isinstance(n_list[0],list):
+                    if O:
+                        rB += n_list[index][i] * B0[i]
+                    else:
+                        rB += n_list[index][i] * B[i]
+                else:
+                    rB += n_list[i] * B[i]
+
+            rxnB.append(round(rB, 7))
+            index += 1
+
+        rxnB_list.append(rxnB)
+
+    rxnB_list = pd.DataFrame(np.array(rxnB_list).reshape(len(P_list), len(T_list))).T
+    return rxnB_list
+
+def Bmix(Aspen, Asp, T_list, P_list, n_list,NCOMP):
+    aspen_tools.cd_properties(Asp)
+    # Asp.set_focus()
+    # Asp.child_window(title="MIX-1", control_type="TreeItem").click_input()
+    B_mix_list = []
+
+    Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\MIX-1\Input\LOWER\#1").Value = T_list[0]
+    Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\MIX-1\Input\UPPER\#1").Value = T_list[-1]
+    Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\MIX-1\Input\NPOINT\#1").Value = len(T_list) - 1
+
+    for pi, P in enumerate(P_list):
+        Aspen.Application.Tree.FindNode(f"\Data\Properties\Analysis\MIX-1\Input\LIST\#0\#{pi}").Value = P
+
+    if isinstance(n_list[0],list):
+        for ni, N in enumerate(n_list):
+            for i in range(NCOMP):
+                Aspen.Application.Tree.FindNode(
+                    f"\Data\Properties\Analysis\MIX-1\Input\FLOW\COMP{i+1}"
+                ).Value = N[i]
+
+            ntot = sum(N)
+            aspen_tools.run(Asp)
+            
+            B_mix = Aspen.Application.Tree.FindNode(
+                r"\Data\Properties\Analysis\MIX-1\Output\Prop Data\PROPTAB"
+            ).Elements.Item("TOTAL AVAILMX", str(ni + 1)).Value
+            B_mix_list.append(round(B_mix * ntot, 7))
+    else:
+        for i in range(NCOMP):
+            Aspen.Application.Tree.FindNode(f"\Data\Properties\Analysis\MIX-1\Input\FLOW\COMP{i+1}").Value = n_list[i]
+        ntot = sum(n_list)
+        aspen_tools.run(Asp)
+        for i in range(len(T_list) * len(P_list)):
+            B_mix = Aspen.Application.Tree.FindNode(r"\Data\Properties\Analysis\MIX-1\Output\Prop Data\PROPTAB").Elements.Item("TOTAL AVAILMX", str(i + 1)).Value
+            B_mix_list.append(round(B_mix * ntot, 7))        
+
+    B_mix_list = pd.DataFrame(np.array(B_mix_list).reshape(len(P_list), len(T_list))).T
+    return B_mix_list
+
+def Xeq(Aspen, Asp, T_list, P_list, n_in):
+    Tl = T_list[0]
+    Tu = T_list[-1]
+    aspen_tools.cd_simulation(Asp)
+    for i, a in enumerate(n_in):
+        Aspen.Application.Tree.FindNode(f"\Data\Streams\FEED\Input\FLOW\MIXED\COMP{i+1}").Value = a
+    for pi, P in enumerate(P_list):
+        Aspen.Application.Tree.FindNode(
+            f"\Data\Model Analysis Tools\Sensitivity\XEQ\Input\LIST\#0\#{pi}"
+        ).Value = P
+    Aspen.Application.Tree.FindNode(
+        r"\Data\Model Analysis Tools\Sensitivity\XEQ\Input\LOWER\#1"
+    ).Value = Tl
+    Aspen.Application.Tree.FindNode(
+        r"\Data\Model Analysis Tools\Sensitivity\XEQ\Input\UPPER\#1"
+    ).Value = Tu
+    Aspen.Application.Tree.FindNode(
+        r"\Data\Model Analysis Tools\Sensitivity\XEQ\Input\NPOINT\#1"
+    ).Value = len(T_list)
+    
+    Asp.child_window(auto_id="MMTabItem_2", control_type="TabItem").select()
+    try:
+        aspen_tools.reset(Asp)
+        aspen_tools.run_sim(Asp)
+        n_out = []
+        for j in range(len(P_list)):
+            for i in range(len(T_list)):
+                n_out_i = []
+                for k in range(len(n_in)):
+                    n_out_i.append(Aspen.Application.Tree.FindNode("\Data\Model Analysis Tools\Sensitivity\XEQ\Output\SENSVAR").Elements.Item(str(j * len(T_list) + i + 1), str(k+3)).Value)
+                n_out.append(n_out_i)
+    except AttributeError:
+        n_out = []
+        for j in range(len(P_list)):
+            for i in range(len(T_list)):
+                n_out_i = []
+                for k in range(len(n_in)):
+                    n_out_i.append(Aspen.Application.Tree.FindNode("\Data\Model Analysis Tools\Sensitivity\XEQ\Output\SENSVAR").Elements.Item(str(j * len(T_list) + i + 1), str(k+3)).Value)
+                n_out.append(n_out_i)   
+    return n_out
+
+def calc_recycle(Aspen, Asp, n_in, n_out):
+    n_inr = []
+    n_outr = []
+    n_out_after_recycle = []
+    k_list=[]
+    for i, a in enumerate(n_in):
+        if a !=0:
+            k_list.append(n_out[0][i]/a)
+    k = min(k_list)
+    k_index = k_list.index(k)
+    rec_index = [i for i, val in enumerate(k_list) if val - k < 1e-6]
+    string_indices = [str(i+1) for i in rec_index]
+    indices_string = ", ".join(string_indices)
+    print(f"COMP {indices_string} can be fully recycled")
+    for i, n_out_i in enumerate(n_out):
+        a = n_out_i[k_index]/(n_in[k_index]- n_out_i[k_index])
+        n_inr_i = [(1+a)*x for x in n_in]
+        n_outr_i = [(1+a)*x for x in n_out_i]
+        n_inr.append(n_inr_i)
+        n_outr.append(n_outr_i)
+        n_out_after_recycle.append([ x - (a * y) for x, y in zip(n_outr_i, n_in)])
+    return n_inr, n_outr, n_out_after_recycle
+       
+
+def calc_work(Aspen, Asp, Aspen_path,T_list, P_list, Tb_list, n_in, n_out,f,NCOMP,recycle=False):
+    if recycle:
+        n_inr, n_outr, n_out = calc_recycle(Aspen, Asp, n_in, n_out)
+        
+    print("Running n_in_Bp...")
+    for attempt in range(3):
+        try:
+            n_in_Bp = Bpure(Aspen, Asp, T_list, P_list, Tb_list, n_in, NCOMP)
+            break
+        except pythoncom.com_error as e:
+            
+            print(f"Attempt {attempt + 1} failed: {e}")
+            time.sleep(5)
+            Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            raise  
+    if recycle:
+        print("Running n_inr_Bp...")
+        for attempt in range(3):
+            try:
+                n_inr_Bp = Bpure(Aspen, Asp, T_list, P_list,Tb_list, n_inr, NCOMP)
+                break
+            except pythoncom.com_error as e:
+                print(f"Attempt {attempt + 1} failed: {e}")
+                time.sleep(5)
+                Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                raise        
+        print("Running n_inr_Bm...")
+        for attempt in range(3):
+            try:
+                n_inr_Bm = Bmix(Aspen, Asp, T_list, P_list, n_inr, NCOMP)
+                break
+            except pythoncom.com_error as e:
+                print(f"Attempt {attempt + 1} failed: {e}")
+                time.sleep(5)
+                Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                raise
+        print("Running n_outr_Bm...")
+        for attempt in range(3):
+            try:
+                n_outr_Bm = Bmix(Aspen, Asp, T_list, P_list, n_outr, NCOMP)
+                break
+            except pythoncom.com_error as e:
+                print(f"Attempt {attempt + 1} failed: {e}")
+                time.sleep(5)
+                Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                raise
+        print("Running n_outr_Bp...")
+        for attempt in range(3):
+            try:
+                n_outr_Bp = Bpure(Aspen, Asp, T_list, P_list, Tb_list,n_outr, NCOMP)
+                break
+            except pythoncom.com_error as e:
+                print(f"Attempt {attempt + 1} failed: {e}")
+                time.sleep(5)
+                Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                raise
+    else:
+        print("Running n_in_Bm...")
+        for attempt in range(3):
+            try:
+                n_in_Bm = Bmix(Aspen, Asp, T_list, P_list, n_in, NCOMP)
+                break
+            except pythoncom.com_error as e:
+                print(f"Attempt {attempt + 1} failed: {e}")
+                time.sleep(5)
+                Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                raise  
+
+        print("Running n_out_Bm...")
+        for attempt in range(3):
+            try:
+                n_out_Bm = Bmix(Aspen, Asp, T_list, P_list, n_out, NCOMP)
+                break
+            except pythoncom.com_error as e:
+                print(f"Attempt {attempt + 1} failed: {e}")
+                time.sleep(5)
+                Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                raise     
+        
+    print("Running n_out_Bp...")
+    for attempt in range(3):
+        try:
+            n_out_Bp = Bpure(Aspen, Asp, T_list, P_list, Tb_list, n_out, NCOMP)
+            break
+        except pythoncom.com_error as e:
+            print(f"Attempt {attempt + 1} failed: {e}")
+            time.sleep(5)
+            Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            raise  
+    print("Running n_out_Bp0...")
+    for attempt in range(3):
+        try:
+            n_out_Bp0 =Bpure(Aspen, Asp, T_list, P_list, Tb_list, n_out, NCOMP, True)
+            break
+        except pythoncom.com_error as e:
+            print(f"Attempt {attempt + 1} failed: {e}")
+            time.sleep(5)
+            Aspen, Asp = aspen_tools.reconnect_aspen(Aspen_path)
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            raise  
+    # n_in_Bp = Bpure(Aspen, Asp, T_list, P_list, Tb_list, n_in, NCOMP)
+    # n_in_Bm = Bmix(Aspen, Asp, T_list, P_list, n_in, NCOMP)
+    # n_out_Bm = Bmix(Aspen, Asp, T_list, P_list, n_out, NCOMP)
+    # n_out_Bp = Bpure(Aspen, Asp, T_list, P_list, Tb_list, n_out, NCOMP)
+    # n_out_Bp0 =Bpure(Aspen, Asp, T_list, P_list, Tb_list, n_out, NCOMP, True)
+                                                    
+    W_HC = n_in_Bp - n_in_Bp.iloc[0,0]
+    if recycle:
+        W_M = n_inr_Bm - n_inr_Bp
+        W_R = n_outr_Bm - n_inr_Bm
+        W_S = n_outr_Bp - n_outr_Bm
+    else:
+        W_M = n_in_Bm - n_in_Bp
+        W_R = n_out_Bm - n_in_Bm
+        W_S = n_out_Bp - n_out_Bm
+
+    W_CD = n_out_Bp0 - n_out_Bp
+    W_HC_sgen = W_HC.applymap(lambda x: x * 1/f if x >= 0 else x*f)
+    W_M_sgen = W_M.applymap(lambda x: x * 1/f if x >= 0 else x*f)
+    W_R_sgen = W_R.applymap(lambda x: x * 1/f if x >= 0 else x*f)
+    W_S_sgen = W_S.applymap(lambda x: x * 1/f if x >= 0 else x*f)
+    W_CD_sgen = W_CD.applymap(lambda x: x * 1/f if x >= 0 else x*f)
+
+    W_total = W_HC_sgen + W_M_sgen + W_R_sgen + W_S_sgen + W_CD_sgen
+    W_rev = W_HC + W_M + W_R + W_S + W_CD
+    return Aspen,Asp,W_total, W_rev, W_HC_sgen, W_M_sgen, W_R_sgen, W_S_sgen, W_CD_sgen
+
+
+def compute_Xeq_df(n_out, n_in, T_list, P_list, comp_index):
+    NT = len(T_list)
+    NP = len(P_list)
+    n_in_comp = n_in[comp_index-1]
+    Xeq = [[0 for _ in range(NP)] for _ in range(NT)]
+    idx = 0
+    for j in range(NP):           # column = P
+        for i in range(NT):       # row = T
+            n_out_comp = n_out[idx][comp_index-1]
+            Xeq[i][j] = (n_in_comp - n_out_comp) / n_in_comp if n_in_comp != 0 else 0
+            idx += 1
+
+    df = pd.DataFrame(Xeq, index=T_list, columns=P_list)
+
+    return df
+
+
+def export_to_excel(filename, T_list, P_list,Tb_list, Xeq,W_total, W_rev, W_HC_sgen, W_M_sgen, W_R_sgen, W_S_sgen, W_CD_sgen,):
+    with pd.ExcelWriter(filename, engine="openpyxl") as writer:
+        sheet_name = "Results"
+        startcol = 0
+        data_dict = {
+            "W_total": W_total,
+            "W_rev": W_rev,
+            "W_HC_sgen": W_HC_sgen,
+            "W_M_sgen": W_M_sgen,
+            "W_R_sgen": W_R_sgen,
+            "W_S_sgen": W_S_sgen,
+            "W_CD_sgen": W_CD_sgen,
+        }
+
+        for name, df in data_dict.items():
+            df.columns = P_list
+            df.index = T_list
+
+            pd.DataFrame([[name]]).to_excel(writer, sheet_name=sheet_name, startcol=startcol, header=False, index=False)
+
+            df.to_excel(writer, sheet_name=sheet_name, startrow=1, startcol=startcol)
+            startcol += len(df.columns) + 4 
+
+        Tb_df = pd.DataFrame(
+            Tb_list,
+            index=[f"{P} atm" for P in P_list],
+            columns=[f"COMP{i+1}" for i in range(len(Tb_list[0]))]
+        )
+        pd.DataFrame([["Tb_list"]]).to_excel(writer, sheet_name=sheet_name, startcol=startcol, header=False, index=False)
+        Tb_df.to_excel(writer, sheet_name=sheet_name, startrow=1, startcol=startcol)
+        startcol += len(Tb_df.columns) + 4
+        Xeq.to_excel(writer, sheet_name=sheet_name, startrow=0, startcol=startcol)
+    wb = load_workbook(filename)
+    ws = wb["Results"]
+
+    font = Font(name="Calibri", bold=False)
+    no_border = Border(
+            left=Side(style=None),
+            right=Side(style=None),
+            top=Side(style=None),
+            bottom=Side(style=None)
+        )
+
+
+    for row in ws.iter_rows():
+        for cell in row:
+            if cell.value is not None:
+                cell.font = font
+                cell.border = no_border
+
+    wb.save(filename)
---
+++ aspen_tools.py
@@ -0,0 +1,224 @@
+import pywinauto.application
+import win32com.client
+import time
+from pywinauto.keyboard import send_keys
+import pywinauto.mouse as mouse
+
+def connect_aspen(file):
+    Aspen = win32com.client.Dispatch("Apwn.Document")
+    Aspen.InitFromFile2(file)
+    Aspen.Visible = 1  # visible
+    Aspen.SuppressDialogs = False  # close dialog
+    app = pywinauto.application.Application(backend="uia").connect(process=Aspen.ProcessId)
+    Asp = app["Dialog"]
+
+    return Aspen, Asp
+
+def reconnect_aspen(Aspen_path):
+    print("Aspen crash detected, reconnecting...") 
+    try:
+        Aspen = win32com.client.Dispatch("Apwn.Document")
+        Aspen.InitFromFile2(Aspen_path)    
+        Aspen.Visible = 1  # visible
+        Aspen.SuppressDialogs = False  # close dialog
+        app = pywinauto.application.Application(backend="uia").connect(process=Aspen.ProcessId)
+        Asp = app["Dialog"]
+        print("Reconnected to Aspen successfully.")
+        return Aspen, Asp
+    except Exception as e:
+        print("Reconnect failed:", e)
+
+
+def close_aspen(Aspen):
+    Aspen.Close()
+    
+def click_components(Asp):
+    Asp.child_window(title="Components", auto_id="igRibbon_btnProperties_Components", control_type="Button").click()
+    
+def cd_simulation(Asp):
+    try:
+        Asp.children()[3].children()[0].children()[7].select()  # 6=Properties 7=Simulation
+    except:
+        time.sleep(1)
+        pywinauto.mouse.click(coords=(100, 830))
+
+def cd_properties(Asp):
+    try:
+        Asp.children()[3].children()[0].children()[6].select()
+    except:
+        time.sleep(1)
+        pywinauto.mouse.click(coords=(100, 780))
+        
+def click_review(Asp):
+    while True:
+        try:
+            Asp.child_window(
+                title="Selection", auto_id="MMTabItem_1", control_type="TabItem"
+            ).child_window(auto_id="MMTabPage_1", control_type="Custom").child_window(
+                title="cmdReview", auto_id="MMCmdButton_6", control_type="Custom"
+            ).child_window(
+                title="Review", auto_id="PART_BUTTON", control_type="Button"
+            ).click()
+        except:
+            time.sleep(0.3)
+            continue
+        break
+
+def reset(Asp):
+    Asp.child_window(auto_id="igRibbon_QuickAccessToolbar_1", control_type="ToolBar").child_window(auto_id="igRibbon_btnReset", control_type="Button").click()
+    # Asp.child_window(auto_id="igRibbon_QuickAccessToolbar_1", control_type="ToolBar").child_window(title="Reset",auto_id="igRibbon_btnPrpertiesReset",control_type="Button").click()
+    Asp.window(title="Reinitialize").child_window(title="OK",auto_id="Button_1",control_type="Button").click()
+    Asp.window(title="Reinitialize").child_window(title="OK",auto_id="btn0",control_type="Button").click()
+
+def run(Asp):
+    try:
+        Asp.child_window(auto_id="igRibbon_QuickAccessToolbar_1", control_type="ToolBar").child_window(title="Run", auto_id="igRibbon_btnRunProp", control_type="Button").click()
+    except:
+        Asp.set_focus()
+        mouse.click(button='left', coords=(190, 8))    
+    time.sleep(2)
+
+        
+    
+def run_sim(Asp):
+    try:
+        Asp.child_window(
+        auto_id="igRibbon_QuickAccessToolbar_1",
+        control_type="ToolBar"
+        ).child_window(
+        title="Run",
+        auto_id="igRibbon_btnRun",
+        control_type="Button"
+        ).click()
+    except:
+        Asp.set_focus()
+        mouse.click(button='left', coords=(230, 75))
+    time.sleep(2)
+    
+
+def click_Find(asp):
+    asp.child_window(title="Find", auto_id="PART_BUTTON", control_type="Button").click()
+
+
+    
+class FindCompounds:
+    def __init__(self, asp):
+        find_compounds_box = asp.window(title="Find Compounds")
+        self.box = find_compounds_box
+    
+    def input_CAS(self, CAS_number):
+        edit_box = self.box.child_window(auto_id="txtContainValue", control_type="Edit")
+        edit_box.set_text(CAS_number)
+    
+    def equal(self):
+        equals_radio = self.box.child_window(auto_id="radioEquals", control_type="RadioButton")
+        equals_radio.click()
+    
+    def find_now(self):
+        find_now_btn = self.box.child_window(title="Find Now", auto_id="btnFind", control_type="Button")
+        find_now_btn.click()
+        time.sleep(1.5)
+        
+    def if_match(self):
+        '''
+        Check if matching hint is displayed.
+        '''
+        time.sleep(1.8)
+        status_bar = self.box.child_window(auto_id="StatusBar_1", control_type="StatusBar")
+
+        text_ctrl = status_bar.child_window(control_type="Text")
+        if text_ctrl.exists():
+            sub_status_text = text_ctrl.window_text()
+            if 'Matches found: 1' in sub_status_text:
+                return True ,'Match'
+            
+            if 'Matches found' in sub_status_text:
+                return True, 'Multiple Matches'
+            
+            elif 'No Match' in sub_status_text:
+                return False, 'No Match'
+            
+            else:
+                return False, 'Not found yet.'
+            
+        else:
+            return False, 'Error'
+
+    def add_comp(self):
+        list_item = self.box.child_window(title="AspenTech.AspenPlus.UnmanagedWrapper.DbankComponentItem", control_type="DataItem", found_index=0)
+        list_item.select()
+        
+        add_selected_comp_btn = self.box.child_window(title="Add selected compounds", auto_id="btnAdd", control_type="Button")
+        add_selected_comp_btn.click()
+    
+    def close_fc(self):
+        close_btn = self.box.child_window(title="Close", auto_id="btnClose", control_type="Button")
+        close_btn.click()
+        
+def click_comp_cell(Asp,index):
+    x = 855
+    base_y = 275
+    step = 25
+    y = base_y + (index - 1) * step
+    Asp.set_focus()
+    time.sleep(0.2)
+    mouse.click(button='left', coords=(x, y))
+
+def click_nrtl():
+    mouse.click(button='left', coords=(115,470))
+    time.sleep(0.2)
+    mouse.click(button='left', coords=(480,105))
+    time.sleep(0.3)
+    mouse.click(button='left', coords=(960,675))
+    time.sleep(2)
+    mouse.click(button='left', coords=(1000,560))
+    
+def input_CAS_COMP_list(Aspen,Asp, CAS_list):
+    results = []
+    n=len(CAS_list)
+    Asp.set_focus()
+    fc = FindCompounds(Asp)
+    time.sleep(10)
+    for i,CAS in enumerate(CAS_list):
+        click_comp_cell(Asp, i+1)
+        click_Find(Asp)
+        fc.equal()
+        fc.input_CAS(CAS)
+        fc.find_now()
+        is_match, status = fc.if_match()
+        results.append((i+1, CAS, f"COMP{i+1}", status))
+        if is_match:
+            fc.add_comp()
+        fc.close_fc()
+        mouse.click(button='left', coords=(725, 535))
+        mouse.click(button='left', coords=(1060, 560))
+    time.sleep(1)    
+    for _ in range(10-n):
+        Aspen.Application.Tree.Data.Components.Specifications.Input.TYPE.Elements.RemoveRow(0,n)
+    print("\n" + "=" * 65)
+    print(f"{'Component':<10} | {'CAS Number':<12} | {'Component ID':<10} | {'Status'}")
+    print("=" * 65)
+    for comp, CAS, id, status in results:
+        print(f"{comp:<10} | {CAS:<12} | {id:<10} | {status}")
+    print("=" * 65)
+    click_nrtl()
+    for i in range(n):
+        Aspen.Application.Tree.FindNode(f"\Data\Properties\Analysis\MIX-1\Input\FLOW\COMP{i+1}").Value = 1
+    Asp.child_window(auto_id="igRibbon_QuickAccessToolbar_1", control_type="ToolBar").child_window(title="Save",auto_id="igRibbon_ButtonTool_3",control_type="Button").click()
+    cd_simulation(Asp)
+    Asp.child_window(auto_id="MMTabItem_2", control_type="TabItem").select()
+    for i in range(10, n, -1):
+            delete_C(Asp, i)
+def delete_C(Asp,n):
+    x = 300
+    base_y = 300
+    step = 25
+    y = base_y + (n - 1) * step
+    Asp.set_focus()
+    time.sleep(0.2)
+    mouse.click(button='left', coords=(x, y))
+    Asp.child_window(title="cmdDelete", control_type="Custom").child_window(auto_id="PART_BUTTON", control_type="Button").click()
+    time.sleep(0.2)
+    Asp.child_window(title="Confirm", control_type="Window").child_window(auto_id="btn0", control_type="Button").click()
+    time.sleep(0.2)
+    Asp.child_window(auto_id="igRibbon_QuickAccessToolbar_1", control_type="ToolBar").child_window(title="Save",auto_id="igRibbon_ButtonTool_3",control_type="Button").click()
